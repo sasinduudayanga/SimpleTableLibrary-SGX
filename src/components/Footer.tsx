b936b258573bf0ee0bc581a2d8e1fb4406ce6832
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2023-11-25 22:23:23 +0530

    update styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer style={{ textAlign: 'center', padding: '20px', borderTop: '1px solid #ddd' }}>
-      <p>&copy; 2023 Your Company. All rights reserved.</p>
+      <p>&copy; 2023 Sasindu Udayanga. All rights reserved.</p>
     </footer>
   );
 };
